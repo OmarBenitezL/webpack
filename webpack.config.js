aa7c1f53149a637bd920c3f1b4edecad8c0e34d7
OmarBenitezL <isaibenitezlopez@gmail.com>
2019-05-20 16:26:15 -0500

    Finished course

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
 const path = require("path");
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: './src/index.js',
     output: {
         filename : "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    optimization:{
+        minimizer: [new UglifyJsPlugin()]
+    },
+    plugins:[new HTMLWebpackPlugin()],
+    devServer:{
+        contentBase: path.join(__dirname, "dist"),
+        port: 9000
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    { loader: 'url-loader' }
+                ]
+            }
+        ]
     }
 }
